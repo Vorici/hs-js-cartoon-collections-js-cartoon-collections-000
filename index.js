280db1744e078b62334b7a74bfb1a0192d34699a
Ledio Bode <l.bode515@gmail.com>
2018-04-25 15:15:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function dwarfRollCall(dwarves) {
+  
+var  dwarvesNum = []
+
+for (var i = 0; i < dwarves.length; i++ ){
+  
+  dwarvesNum.push ([i + 1] + ". " + dwarves[i])
+}
+  
+  return dwarvesNum;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  
+  for(var i=0; i<planeteerCalls.length; i++){
+    planeteerCalls[i]=planeteerCalls[i] + '!';
+}
+
+  return planeteerCalls.map(function(x){ return x.toUpperCase() })
+  
 }
 
 function longPlaneteerCalls(words) {
